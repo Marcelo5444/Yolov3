cd768d43999cef98a175129c2c11d081a234417d
Marcelo5444 <marcelosanchezortega@gmail.com>
2020-02-06 23:29:15 +0100

    new changes

--- kmeans_Anchors.py
+++ kmeans_Anchors.py
@@ -2,31 +2,34 @@ import numpy as np
 import numpy as np
 import pandas as pd
 import os
+
 import cv2
 import matplotlib.patches as patches
 from PIL import Image
 import matplotlib.pyplot as plt
 from utils import *
 
+def add_patch(ax,x_min,y_min,x_max,y_max,color):
+	ax.add_patch(patches.Rectangle((x_min,y_min),np.abs(x_min-x_max),np.abs(y_max-y_min),linewidth=2,edgecolor=color,facecolor='none'))
 def IoU(boxA,boxB):
 	xA = max(boxA[0], boxB[0])
 	yA = max(boxA[1], boxB[1])
 	xB = min(boxA[2], boxB[2])
 	yB = min(boxA[3], boxB[3])
- 
+
 	# compute the area of intersection rectangle
 	interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
- 
+
 	# compute the area of both the prediction and ground-truth
 	# rectangles
 	boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
 	boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
- 
+
 	# compute the intersection over union by taking the intersection
 	# area and dividing it by the sum of prediction + ground-truth
 	# areas - the interesection area
 	iou = interArea / float(boxAArea + boxBArea - interArea)
- 
+
 	# return the intersection over union value
 	return iou
 def IoU_fixed(w1,h1,w2,h2):
@@ -51,12 +54,12 @@ def update_anchors(anchors,data):
             mean = np.mean(data[data[:,2]==i],axis=0)[:2]
             for item in mean:
                 if np.isnan(item) or item <0:
-                    mean[item] = anchors[i,item]             
+                    mean[item] = anchors[i,item]
             anchors[i,:] = mean
 
     return anchors
 
-def kmeans_anchors(data,iterations=10,num_anchors=4,resolution=(1200,400)):
+def kmeans_anchors(data,iterations=10,num_anchors=4,resolution=(1200,400),plot=False):
     width = np.random.randint(low=0,high=1200,size=num_anchors)
     height = np.random.randint(low=0,high=400,size=num_anchors)
     anchors = np.column_stack((width,height))
@@ -64,18 +67,18 @@ def kmeans_anchors(data,iterations=10,num_anchors=4,resolution=(1200,400)):
     for i in range(0,iterations):
         for iterator in range(0,data.shape[0]):
             score_group = 0.0
-            
+
             final_group = 0.0
             for anchor_iter in range(0,num_anchors):
-                score = IoU_fixed(data[iterator][0],data[iterator][1],anchors[anchor_iter][0],anchors[anchor_iter][1])                 
-            
+                score = IoU_fixed(data[iterator][0],data[iterator][1],anchors[anchor_iter][0],anchors[anchor_iter][1])
+
                 if score > score_group:
                     final_group = anchor_iter
                     score_group  = score
                 if i == iterations-1:
                     sum_of_scores = score_group + sum_of_scores
-            data[iterator][2] = final_group     
-        
+            data[iterator][2] = final_group
+
         anchors = update_anchors(anchors,data)
         display_anchors(anchors)
 
@@ -87,14 +90,16 @@ def display_anchors(anchors):
     fig1 = plt.figure()
     ax1 = fig1.add_subplot(111, aspect='equal')
     for i in range(0,anchors.shape[0]):
-        ax1.add_patch(
-        patches.Rectangle((0, 0), anchors[i,0], anchors[i,1],fill=False))
+
+         plot_centered_Anchor(ax1,(250,250),anchors[i,0],anchors[i,1],"r")
     plt.ylim(lims)
     plt.xlim(lims)
-    
 
- def display_Anchors_and_BB()       
-    
-    
-    
-        
+
+def display_Anchors_and_BB():
+    return
+def plot_centered_Anchor(axis,centered,width,height,color):
+
+         x = centered[0]-width/2
+         y = centered[1]-height/2
+         add_patch(axis,x,y,x+width,y+height,color)
--- utils.py
+++ utils.py
@@ -5,14 +5,20 @@ import cv2
 import matplotlib.patches as patches
 from PIL import Image
 import matplotlib.pyplot as plt
+from kmeans_Anchors import *
 
 def add_patch(ax,x_min,y_min,x_max,y_max,color):
 	ax.add_patch(patches.Rectangle((x_min,y_min),np.abs(x_min-x_max),np.abs(y_max-y_min),linewidth=2,edgecolor=color,facecolor='none'))
 
+def plot_centered_Anchor(axis,centered,width,height,color):
+
+		x = centered[0]-width/2
+		y = centered[1]-height/2
+		add_patch(axis,x,y,x+width,y+height,color)
 def display_bounding_boxes(df,path_to_images=None,path_to_labels=None,specific_image=None):
 	if path_to_labels == None:
 		path_to_labels = "/home/mlrig/Documents/Yolo3 Implementation/data/training/label_2//"
-	if path_to_images == None:	
+	if path_to_images == None:
 		path_to_images = "/home/mlrig/Documents/Yolo3 Implementation/data/training/image_2//"
 	"""""Display the image with the label bounding box over it,You can also specify the concrete image """
 	if specific_image is None:
@@ -20,8 +26,8 @@ def display_bounding_boxes(df,path_to_images=None,path_to_labels=None,specific_i
 		file = data["image"]
 		new_df  = df.loc[df["image"]==file]
 		path = os.path.join(path_to_images,str(data["image"]))+".png"
-		
-	if specific_image!=None:	
+
+	if specific_image!=None:
 		new_df = df.loc[df["image"]==specific_image]
 		path = os.path.join(path_to_images,specific_image)+".png"
 
@@ -31,30 +37,83 @@ def display_bounding_boxes(df,path_to_images=None,path_to_labels=None,specific_i
 
 	ax.imshow(im)
 	for i,j in new_df.iterrows():
- 			x_min = float(j["x_min"])
- 			y_min = float(j["y_min"])
- 			x_max = float(j["x_max"])
- 			y_max  = float(j["y_max"])
- 			if j["type"]=="Vehicle":
- 				color = 'r'
- 			elif j["type"]=="DontCare":
- 				color = 'g'
- 			elif j["type"]=="Pedestrian":
- 			 	color = 'b'
- 			elif j["type"]=="Misc":
- 				color = "y"
- 			elif j["type"]=="Cyclist":
- 				color  = "w"
- 			elif j["type"]=="Tram":
- 				color = 'k'				
- 			add_patch(ax,x_min,y_min,x_max,y_max,color)
-	plt.show() 			
+			x_min = j["x_min"]
+			y_min = j["y_min"]
+			x_max = j["x_max"]
+			y_max  = j["y_max"]
+			if j["type"]=="Vehicle":
+				color = 'r'
+			elif j["type"]=="DontCare":
+				color = 'g'
+			elif j["type"]=="Pedestrian":
+				color = 'b'
+			elif j["type"]=="Misc":
+				color = "y"
+			elif j["type"]=="Cyclist":
+				color  = "w"
+			elif j["type"]=="Tram":
+				color = 'k'
+			add_patch(ax,x_min,y_min,x_max,y_max,color)
+	plt.show()
+
+def display_bounding_boxes_with_anchors(df,anchors_array,path_to_images=None,path_to_labels=None,specific_image=None):
+	if path_to_labels == None:
+		path_to_labels = "/home/mlrig/Documents/Yolo3 Implementation/data/training/label_2//"
+	if path_to_images == None:
+		path_to_images = "/home/mlrig/Documents/Yolo3 Implementation/data/training/image_2//"
+	"""""Display the image with the label bounding box over it,You can also specify the concrete image """
+	if specific_image is None:
+		data = df.loc[np.random.randint(0,df.shape[0])]
+		file = data["image"]
+		new_df  = df.loc[df["image"]==file]
+		path = os.path.join(path_to_images,str(data["image"]))+".png"
+
+	if specific_image!=None:
+		new_df = df.loc[df["image"]==specific_image]
+		path = os.path.join(path_to_images,specific_image)+".png"
+
+	im = np.array(Image.open(path), dtype=np.uint8)
+	fig,ax = plt.subplots(1)
+	print("path to the image",path)
+	ax.imshow(im)
+	score_group = 0.0
+	final_group = 0.0
+	for i,j in new_df.iterrows():
+			x_min = j["x_min"]
+			y_min = j["y_min"]
+			x_max = j["x_max"]
+			y_max  = j["y_max"]
+			if j["type"]=="Vehicle":
+				color = 'r'
+			elif j["type"]=="DontCare":
+				color = 'g'
+			elif j["type"]=="Pedestrian":
+				color = 'b'
+			elif j["type"]=="Misc":
+				color = "y"
+			elif j["type"]=="Cyclist":
+				color  = "w"
+			elif j["type"]=="Tram":
+				color = 'k'
+			final_group = 0
+			score_group  = 0
+
+			for anchor_iter in range(0,anchors_array.shape[0]):
+				score = IoU_fixed(j["width"],j["height"],anchors_array[anchor_iter][0],anchors_array[anchor_iter][1])
+				if score > score_group:
+					final_group = anchor_iter
+			center = (x_min+0.5*j["width"],y_min+0.5*j["height"])
+			print(anchors_array[final_group][0])
+			plot_centered_Anchor(ax,center,j["width"],j["height"],"b")
+			plot_centered_Anchor(ax,center,anchors_array[final_group][0],anchors_array[final_group][1],"r")		
+plt.show()
+
 
 def create_label_datframe(path_to_labels=None):
 	if path_to_labels == None:
 		path_to_labels = "/home/mlrig/Documents/Yolo3 Implementation/data/training/label_2//"
 	labels = os.listdir(path_to_labels)
-	d = {"image":[],"type":[],"x_min":[],"y_min":[],"x_max":[],"y_max":[],"height":[],"width":[]}	
+	d = {"image":[],"type":[],"x_min":[],"y_min":[],"x_max":[],"y_max":[],"height":[],"width":[]}
 	for label in labels:
 		f = open(os.path.join(path_to_labels,label), "r")
 		data = f.read()
@@ -78,4 +137,4 @@ def create_label_datframe(path_to_labels=None):
 				heigth  = np.abs(float(register[5])-float(register[7]))
 				d["height"].append(heigth)
 	df = pd.DataFrame(data=d)
-	return df		
+	return df
